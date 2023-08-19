d9fa634703a9f15e7e999ee4b201b116f769f9ca
Mark Goudie <goudie.mark@gmail.com>
2023-08-19 13:24:19 +1000

    superuser and models added

---
+++ ProjectNotes.md
@@ -0,0 +1,7 @@
+First user: John Doe
+email: john.doe@foo.com
+password: password1
+
+Superuser: admin1
+email: admin1@foo.com
+password: password0
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,37 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    image = models.URLField(blank=True)
+    category = models.CharField(max_length=50, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Bid by {self.bidder.username} on {self.listing.title}"
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.commenter.username} on {self.listing.title}"
