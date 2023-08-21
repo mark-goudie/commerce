462e7478b54ec7a3574b36d54b5e344a48a028f0
Mark Goudie <goudie.mark@gmail.com>
2023-08-21 22:01:57 +1000

    specifications drafted

--- auctions/models.py
+++ auctions/models.py
@@ -1,22 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     pass
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
-    current_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    current_bid = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0)
     image = models.URLField(blank=True)
     category = models.CharField(max_length=50, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     closed = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
 
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
@@ -27,6 +32,7 @@ class Bid(models.Model):
     def __str__(self):
         return f"Bid by {self.bidder.username} on {self.listing.title}"
 
+
 class Comment(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     commenter = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -35,3 +41,17 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.commenter.username} on {self.listing.title}"
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="watchlist")
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="watchlist")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid, Comment, Watchlist, Category
+from .forms import ListingForm
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(active=True)
+    return render(request, "auctions/index.html", {"active_listings": active_listings})
 
 
 def login_view(request):
@@ -61,3 +63,76 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            new_listing = form.save(commit=False)
+            new_listing.owner = request.user
+            new_listing.save()
+            return redirect('listings')
+    else:
+        form = ListingForm()
+
+    return render(request, 'auctions/create_listing.html', {'form': form})
+
+
+def listing_detail(request, listing_id):
+    try:
+        listing = Listing.objects.get(pk=listing_id)
+    except Listing.DoesNotExist:
+        return HttpResponseBadRequest("Listing not found")
+
+    current_bid = Bid.objects.filter(
+        listing=listing).aggregate(Max("amount"))["amount__max"]
+
+    if request.method == "POST":
+        action = request.POST.get("action")
+        if action == "watchlist_toggle":
+            Watchlist.toggle_watchlist(request.user, listing)
+        elif action == "place_bid":
+            bid_amount = float(request.POST.get("bid_amount"))
+            if current_bid is None or bid_amount > current_bid:
+                Bid.objects.create(
+                    listing=listing, user=request.user, amount=bid_amount)
+                listing.current_price = bid_amount
+                listing.save()
+            else:
+                return HttpResponseBadRequest("Invalid bid amount")
+        elif action == "close_auction":
+            if request.user == listing.owner and listing.is_active:
+                winning_bid = Bid.objects.filter(
+                    listing=listing).order_by("-amount").first()
+                if winning_bid:
+                    listing.winner = winning_bid.user
+                    listing.is_active = False
+                    listing.save()
+        elif action == "add_comment":
+            comment_text = request.POST.get("comment_text")
+            Comment.objects.create(
+                listing=listing, user=request.user, text=comment_text)
+
+    return render(request, "auctions/listing_detail.html", {
+        "listing": listing,
+        "current_bid": current_bid,
+        "user_watchlist": listing.watchlist.filter(user=request.user),
+    })
+
+
+def watchlist(request):
+    user = request.user
+    watchlist_items = user.watchlist.all()
+    return render(request, "auctions/watchlist.html", {"watchlist_items": watchlist_items})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, "auctions/category_list.html", {"categories": categories})
+
+
+def category_listings(request, category_id):
+    category = Category.objects.get(id=category_id)
+    listings = Listing.objects.filter(category=category, closed=False)
+    return render(request, "auctions/category_listings.html", {"category": category, "listings": listings})
